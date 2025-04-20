1dc1a442baad3bc69c33ccdb61b25b26eae6eb2d
agsb <barcellos.alvaro@gmail.com>
2025-04-19 21:24:38 -0300

    does wire-wraps list sorted

--- doitwwp.sh
+++ doitwwp.sh
@@ -1,3 +1,8 @@
+#/bin/bash
+#
+#   agsb@2025
+#
+#   make a list of pins conections for wire wrap
 #
 #   premisse:
 #   each wire have one name,
@@ -10,16 +15,19 @@
 #
 #   notes:
 #
-#   one line for each wire nameon pin,
+#   one line for each wire on pin,
 #   use obs for datasheet use of pin,
+#   use unit, 00, 00, x, y, for size of unit
+#
+#   use nc for not connected
 #   use 0V0, for VSS, gnd
 #   use 3V3, for VDD,
 #   use 5V5, for VCC,
 #
 cat $1 | \
-sort -k 3 -t',' | \
+sort -t',' -k3,3 -k1,2 | \
 sed -e '/^[ ]*$/d' | \
-tee $1.pts | \
+tee $1.pns | \
 grep -v '# ' | grep -v ' nc,' | \
 cat -n > $1.lst
 
